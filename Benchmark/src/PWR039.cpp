7d1019157c55b5c9ddc79e4920944bad6bd9c09d
Daniel Otero <daniel.otero@codee.com>
2024-01-22 19:54:21 +0100

    PWR039: Add reproducibility for Fortran example

--- Benchmark/src/PWR039.cpp
+++ Benchmark/src/PWR039.cpp
@@ -4,6 +4,8 @@
 extern "C" {
 void matmul(int n, const double *A, const double *B, double *C);
 void matmul_improved(int n, const double *A, const double *B, double *C);
+void matmul_f(int n, double *A, double *B, double *C);
+void matmul_improved_f(int n, double *A, double *B, double *C);
 }
 
 // Size adjusted to fit execution on micro-seconds
@@ -35,5 +37,32 @@ static void CImprovedBench(benchmark::State &state) {
 
 OC_BENCHMARK("PWR039 C Example", CExampleBench);
 OC_BENCHMARK("PWR039 C Improved", CImprovedBench);
+
+#endif
+
+#if OCB_ENABLE_Fortran
+
+static void FortranExampleBench(benchmark::State &state) {
+  auto A = OpenCatalog::CreateRandomVector<double>(N * N);
+  auto B = OpenCatalog::CreateRandomVector<double>(N * N);
+  auto C = OpenCatalog::CreateZeroVector<double>(N * N);
+  for (auto _ : state) {
+    matmul_f(N, A.data(), B.data(), C.data());
+    benchmark::DoNotOptimize(C);
+  }
+}
+
+static void FortranImprovedBench(benchmark::State &state) {
+  auto A = OpenCatalog::CreateRandomVector<double>(N * N);
+  auto B = OpenCatalog::CreateRandomVector<double>(N * N, 1.0, 2.0);
+  auto C = OpenCatalog::CreateZeroVector<double>(N * N);
+  for (auto _ : state) {
+    matmul_improved_f(N, A.data(), B.data(), C.data());
+    benchmark::DoNotOptimize(C);
+  }
+}
+
+OC_BENCHMARK("PWR039 Fortran Example", FortranExampleBench);
+OC_BENCHMARK("PWR039 Fortran Improved", FortranImprovedBench);
 
 #endif
