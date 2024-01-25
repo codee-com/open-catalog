fdefe5140e61bb832b54484113b4578bc62aa81c
Iñaki Amatria Barral <inaki.amatria@appentra.com>
2024-01-25 08:38:08 +0100

    PWR063: Update the check example
    This check example is more realistic than the one we tested before.

--- Checks/PWR063/example.f
+++ Checks/PWR063/example.f
@@ -1,5 +1,27 @@
 ! PWR063: avoid using legacy Fortran constructs
 
-      SUBROUTINE example()
-        PAUSE 1
-      END SUBROUTINE example
+      PROGRAM LegacyFortran
+        INTEGER A, B, C, X, I
+        COMMON /MyCommonBlock/ A, B, C
+        DATA A /10/, B /20/, C /30/, X /0/, I /1/
+
+10      CONTINUE
+        CALL UpdateValue(X)
+        WRITE(*,*) "Update X =", X
+        I = I + 1
+        IF (I - 11) 10, 20, 30
+
+20      CONTINUE
+        WRITE(*,*) "Final  X =", X
+        STOP
+
+30      CONTINUE
+        WRITE(*,*) "Error: loop exceeded 10 iterations"
+        STOP
+      END PROGRAM LegacyFortran
+
+      SUBROUTINE UpdateValue(X)
+        INTEGER A, B, C, X
+        COMMON /MyCommonBlock/ A, B, C
+        X = X + A + B + C
+      END SUBROUTINE UpdateValue
