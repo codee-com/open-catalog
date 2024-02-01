f1b3bb923937d1b4f9adcdd1a93331d9fa76d871
Iñaki Amatria Barral <inaki.amatria@appentra.com>
2024-02-01 12:12:00 +0100

    PWR022: Add reproducibility to the Fortran example

--- Benchmark/src/PWR022.cpp
+++ Benchmark/src/PWR022.cpp
@@ -6,6 +6,8 @@ void example(int n, const double *A, const double *B, const double *C,
              double *D);
 void solution(int n, const double *A, const double *B, const double *C,
               double *D);
+void example_f(int n, double *A, double *B, double *C, double *D);
+void solution_f(int n, double *A, double *B, double *C, double *D);
 }
 
 // Size adjusted to fit execution on micro-seconds
@@ -37,5 +39,34 @@ static void CImprovedBench(benchmark::State &state) {
 
 OC_BENCHMARK("PWR022 C Example", CExampleBench);
 OC_BENCHMARK("PWR022 C Improved", CImprovedBench);
+
+#endif
+
+#if OCB_ENABLE_Fortran
+
+static void FortranExampleBench(benchmark::State &state) {
+  auto A = OpenCatalog::CreateRandomVector<double>(N);
+  auto B = OpenCatalog::CreateRandomVector<double>(N * N);
+  auto C = OpenCatalog::CreateRandomVector<double>(N * N * N);
+  auto D = OpenCatalog::CreateUninitializedVector<double>(N * N);
+  for (auto _ : state) {
+    example_f(N, A.data(), B.data(), C.data(), D.data());
+    benchmark::DoNotOptimize(D);
+  }
+}
+
+static void FortranImprovedBench(benchmark::State &state) {
+  auto A = OpenCatalog::CreateRandomVector<double>(N);
+  auto B = OpenCatalog::CreateRandomVector<double>(N * N);
+  auto C = OpenCatalog::CreateRandomVector<double>(N * N * N);
+  auto D = OpenCatalog::CreateUninitializedVector<double>(N * N);
+  for (auto _ : state) {
+    solution_f(N, A.data(), B.data(), C.data(), D.data());
+    benchmark::DoNotOptimize(D);
+  }
+}
+
+OC_BENCHMARK("PWR022 Fortran Example", FortranExampleBench);
+OC_BENCHMARK("PWR022 Fortran Improved", FortranImprovedBench);
 
 #endif
--- Checks/PWR022/solution.f90
+++ Checks/PWR022/solution.f90
@@ -1,14 +1,16 @@
 ! PWR022: move invariant conditional out of the loop to facilitate vectorization
 
-subroutine solution_f(n, A, B, C, D)
+subroutine solution_f(n, A, B, C, D) bind(c)
+  use iso_c_binding, only : c_int, c_double
+
   implicit none
-  integer i, j, k
-  integer, intent(in) :: n
-  real :: val
-  real, dimension(1:n), intent(in) :: A
-  real, dimension(1:n, 1:n), intent(in) :: B
-  real, dimension(1:n, 1:n, 1:n), intent(in) :: C
-  real, dimension(1:n, 1:n), intent(out) :: D
+  integer(kind=c_int) i, j, k
+  integer(kind=c_int), intent(in), value :: n
+  real(kind=c_double) :: val
+  real(kind=c_double), dimension(1:n), intent(in) :: A
+  real(kind=c_double), dimension(1:n, 1:n), intent(in) :: B
+  real(kind=c_double), dimension(1:n, 1:n, 1:n), intent(in) :: C
+  real(kind=c_double), dimension(1:n, 1:n), intent(out) :: D
 
   do k = 1, n
     do j = 1, n
