905203393ca99eacf8343886b0d6994bf61a27c9
Iñaki Amatria Barral <inaki.amatria@appentra.com>
2024-02-01 12:05:52 +0100

    PWR022: Improve the C and Fortran examples

--- Checks/PWR022/example.c
+++ Checks/PWR022/example.c
@@ -1,13 +1,14 @@
 // PWR022: move invariant conditional out of the loop to facilitate
 // vectorization
 
-int example(int *A, int n) {
-  int total = 0;
-  for (int i = 0; i < n; i++) {
-    if (n < 10) {
-      total++;
+void example(int n, const double *A, const double *B, const double *C,
+             double *D) {
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      for (int k = 0; k < n; ++k) {
+        const double val = i == 0 ? B[j * n + k] : D[j * n + k];
+        D[j * n + k] = val + A[i] * C[i * n * n + j * n + k];
+      }
     }
-    A[i] = total;
   }
-  return total;
 }
--- Checks/PWR022/example.f90
+++ Checks/PWR022/example.f90
@@ -1,16 +1,25 @@
 ! PWR022: move invariant conditional out of the loop to facilitate vectorization
 
-subroutine example()
+subroutine example_f(n, A, B, C, D)
   implicit none
-  integer :: n
-  integer :: a(1000), i, total
-
-  n = 100
+  integer i, j, k
+  integer, intent(in) :: n
+  real :: val
+  real, dimension(1:n), intent(in) :: A
+  real, dimension(1:n, 1:n), intent(in) :: B
+  real, dimension(1:n, 1:n, 1:n), intent(in) :: C
+  real, dimension(1:n, 1:n), intent(out) :: D
 
   do i = 1, n
-    if (n .lt. 10) then
-      total = total + 1
-    end if
-    a(i) = total
+    do k = 1, n
+      do j = 1, n
+        if (i .eq. 1) then
+          val = B(j, k)
+        else
+          val = D(j, k)
+        end if
+        D(j, k) = val + A(i) * C(j, k, i)
+      end do
+    end do
   end do
-end subroutine example
+end subroutine example_f
