4d818221fcf80584ce6a67d01574ffcf983b73d3
Iñaki Amatria Barral <inaki.amatria@appentra.com>
2024-05-13 13:57:39 +0200

    PWR003: Simplify the Fortran example
    We decided to use a separate file for the module because we thought that
    moving the implementation to a different TU would allow hoisting of
    `gravityOf` when marked with `pure`. This is not the case. `pure` has a
    complete different meaning in Fortran. Let us therefore simplify the
    example and use one source for the example and one source for the
    solution.

--- Checks/PWR003/solution.f90
+++ Checks/PWR003/solution.f90
@@ -1,9 +1,45 @@
 ! PWR003: Explicitly declare pure functions
 
+module gravitySolution
+  use iso_c_binding, only : c_int
+  implicit none
+  integer(kind=c_int), parameter :: mercury = 0, venus = 1, earth = 2, &
+                                    mars = 3, jupiter = 4, saturn = 5, &
+                                    uranus = 6, neptune = 7
+contains
+  pure function gravityOf(planet)
+    use iso_c_binding, only: c_double
+
+    real(kind=c_double) :: gravityOf
+    integer(kind=c_int), intent(in) :: planet
+
+    select case (planet) 
+      case (mercury)
+        gravityOf = 3.7
+      case (venus)
+        gravityOf = 8.9
+      case (earth)
+        gravityOf = 9.8
+      case (mars)
+        gravityOf = 3.7
+      case (jupiter)
+        gravityOf = 23.1
+      case (saturn)
+        gravityOf = 9.0
+      case (uranus)
+        gravityOf = 8.7
+      case (neptune)
+        gravityOf = 11.0
+      case default
+        gravityOf = 0.0
+    end select
+  end function gravityOf
+end module gravitySolution
+
 ! Computes the weight of each object in a vector
-subroutine solution_f(n, M, W) bind(c)
+pure subroutine solution_f(n, M, W) bind(c)
   use iso_c_binding, only : c_int, c_double
-  use gravity_getters, only : gravity_pure
+  use gravitySolution, only : mars, gravityOf
 
   implicit none
   integer(kind=c_int) :: i
@@ -12,6 +48,6 @@ subroutine solution_f(n, M, W) bind(c)
   real(kind=c_double), dimension(1:n), intent(out) :: W
 
   do i = 1, n
-    W(i) = M(i) * gravity_pure(3)
+    W(i) = M(i) * gravityOf(mars)
   end do
 end subroutine solution_f
