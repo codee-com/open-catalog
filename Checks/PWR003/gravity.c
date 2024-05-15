0370b9a1340375926450f9cb3e48af90d789ac88
Rui Marques <rui.marques@appentra.com>
2024-05-15 16:36:49 +0100

    PWR003: Simplify C example

--- Checks/PWR003/gravity.c
+++ Checks/PWR003/gravity.c
@@ -1,23 +1,21 @@
 // Returns the gravity on planet `planetIdx` in m/s2
 double gravity(const int planetIdx) {
-  switch (planetIdx) {
-  case 0: // Mercury
+  if (planetIdx == 0) // Mercury
     return 3.7;
-  case 1: // Venus
+  if (planetIdx == 1) // Venus
     return 8.9;
-  case 2: // Earth
+  if (planetIdx == 2) // Earth
     return 9.8;
-  case 3: // Mars
+  if (planetIdx == 3) // Mars
     return 3.7;
-  case 4: // Jupiter
+  if (planetIdx == 4) // Jupiter
     return 23.1;
-  case 5: // Saturn
+  if (planetIdx == 5) // Saturn
     return 9.0;
-  case 6: // Uranus
+  if (planetIdx == 6) // Uranus
     return 8.7;
-  case 7: // Neptune
+  if (planetIdx == 7) // Neptune
     return 11.0;
-  default:
-    return 0.0;
-  }
+
+  return 0.0;
 }
