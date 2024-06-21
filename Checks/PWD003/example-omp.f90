f982a28e82fea6b4d93835c0570aaa9365ad10bc
alvrogd <alvaro.dieste@appentra.com>
2024-06-21 09:50:11 +0200

    PWD003: Update Fortran code to assumed-size arrays
    Since the `README.md` entry explicitly discusses them.

--- Checks/PWD003/example-omp.f90
+++ Checks/PWD003/example-omp.f90
@@ -1,18 +1,19 @@
 ! PWD003: Copy of pointer value instead of pointed-to data to an accelerator
 ! device
 
-subroutine example(a, b, result, size)
+subroutine example(a, b, sum, size)
   implicit none
+  integer, dimension(*), intent(in) :: a, b
+  integer, dimension(*), intent(out) :: sum
   integer, intent(in) :: size
-  integer, dimension(1:size), intent(in) :: a, b
-  integer, dimension(1:size), intent(out) :: result
   integer :: i
 
-  ! array bounds should be specified
-  !$omp target map(to: a, b) map(from: result)
-  !$omp parallel do default(none) shared(a, b, result)
+  ! Array bounds should be specified
+  !$omp target map(to: a, b) map(from: sum)
+  !$omp parallel do default(none) shared(a, b, sum)
   do i = 1, size
-    result(i) = a(i) + b(i)
+    sum(i) = a(i) + b(i)
   end do
+  !$omp end parallel do
   !$omp end target
 end subroutine example
