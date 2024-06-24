fea119808c2f0cbbea0e1d8532ea967ae5d1e04d
alvrogd <alvaro.dieste@appentra.com>
2024-06-24 14:53:41 +0200

    PWR040: Use `i` -> `j` indexing in Fortran
    Just like the C code example. Use more specific argument intents as
    well.

--- Checks/PWR040/example.f90
+++ Checks/PWR040/example.f90
@@ -1,14 +1,14 @@
 ! PWR040: Consider loop tiling to improve the locality of reference
 
-subroutine example(a, b, n)
+subroutine example(a, b)
   implicit none
-  integer, intent(in) :: n
-  real, dimension(1:n, 1:n), intent(inout) :: a, b
+  real, dimension(:, :), intent(out) :: a
+  real, dimension(:, :), intent(in) :: b
   integer :: i, j
 
-  do i = 1, n
-    do j = 1, n
-      a(j, i) = b(i, j)
+  do j = 1, size(a, 2)
+    do i = 1, size(a, 1)
+      a(i, j) = b(j, i)
     end do
   end do
 end subroutine example
