24146d9df99dd9be55b10ba342de9cf90ef9beed
alvrogd <alvaro.dieste@appentra.com>
2024-06-24 10:53:44 +0200

    PWR009: Update Fortran code to match README
    So that it performs a matrix multiplication computation, just like
    PWR009 entry describes.

--- Checks/PWR009/example-omp.f90
+++ Checks/PWR009/example-omp.f90
@@ -1,18 +1,22 @@
 ! PWR009: Use OpenMP teams to offload work to GPU
 
-subroutine example()
+subroutine example(A, B, C)
   implicit none
-  real, dimension(100,200) :: y
-  integer(8) :: i, j
+  real, dimension(:, :), intent(in) :: A, B
+  real, dimension(:, :), intent(inout) :: C
+  integer :: i, j, k
 
-  !$omp target map(from: y(1:100, 1:200))
-  !$omp parallel default(none) private(i, j) shared(y)
-  !$omp do private(j) schedule(static)
-  do i = 1, 100
-    do j = 1, 200
-      y(i, j) = 0
+  !$omp target map(to: A, B) map(tofrom: C)
+  !$omp parallel default(none) private(i, j, k) shared(A, B, C)
+  !$omp do
+  do j = 1, size(C, 2)
+    do k = 1, size(C, 2)
+      do i = 1, size(C, 1)
+        C(i, j) = C(i, j) + A(i, k) * B(k, j)
+      end do
     end do
   end do
+  !$omp end do
   !$omp end parallel
   !$omp end target
 end subroutine example
