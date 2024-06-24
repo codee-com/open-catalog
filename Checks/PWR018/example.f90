0440918daec92c0d686fe0dc992d296c2afbc9a9
alvrogd <alvaro.dieste@appentra.com>
2024-06-24 13:36:12 +0200

    PWR018: Correct Fortran code example
    F(0) is 0. Contain `fibonacci()` within a module to avoid the implicit
    interface.

--- Checks/PWR018/example.f90
+++ Checks/PWR018/example.f90
@@ -1,20 +1,29 @@
 ! PWR018: Call to recursive function within a loop may inhibit vectorization
 
-recursive function fibonacci(n) result(fibo)
-  implicit none
-  integer, intent(in) :: n
-  integer :: fibo
-  if (n <= 2) then
-    fibo = 1
-  else
-    fibo = fibonacci(n - 1) + fibonacci(n - 2)
-  end if
-end function fibonacci
+module mod_fibonacci
+  contains
+  recursive function fibonacci(n) result(fibo)
+    implicit none
+    integer, intent(in) :: n
+    integer :: fibo
+
+    if (n == 0) then
+      fibo = 0
+    else if (n == 1) then
+      fibo = 1
+    else
+      fibo = fibonacci(n - 1) + fibonacci(n - 2)
+    end if
+  end function fibonacci
+end module mod_fibonacci
 
 subroutine example(times)
+  use mod_fibonacci, only : fibonacci
+
   implicit none
-  integer :: sum, fibonacci
-  integer :: i, times
+  integer, intent(in) :: times
+  integer :: i, sum
+
   do i = 1, times
     sum = sum + fibonacci(i)
   end do
