d07d4a1a62ed42328501c986567c49d907dd16cc
alvrogd <alvaro.dieste@appentra.com>
2024-06-24 14:21:08 +0200

    PWR035: Use `i` -> `j` indexing in Fortran
    Just like the C code example. The updated code continues to perform
    repeated accesses in one of the two dimensions (a single row),
    thus triggering PWR035.

--- Checks/PWR035/example.f90
+++ Checks/PWR035/example.f90
@@ -1,12 +1,13 @@
 ! PWR035: Avoid non-consecutive array access to improve performance
 
-subroutine example()
+subroutine example(a)
   implicit none
-  integer :: a(100, 100), i, j
+  integer, intent(out) :: a(:, :)
+  integer :: i, j
 
-  do i = 1, 100
-    do j = 1, 100
-      a(j, 1) = 0
+  do j = 1, size(a, 2)
+    do i = 1, size(a, 1)
+      a(1, j) = 0
     end do
   end do
 end subroutine example
