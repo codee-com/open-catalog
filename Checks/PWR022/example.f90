1e62cd98dadfc90f663c8140c1cfd5c3219c22c1
alvrogd <alvaro.dieste@appentra.com>
2024-06-24 17:16:48 +0200

    PWR022: Correct argument intents
    Use as well the usual `i` -> `j` -> `k` indexing, like the C code
    example. The loops are ordered to ensure optimal memory accesses.

--- Checks/PWR022/example.f90
+++ Checks/PWR022/example.f90
@@ -4,23 +4,23 @@ subroutine example_f(n, A, B, C, D) bind(c)
   use iso_c_binding, only : c_int, c_double
 
   implicit none
-  integer(kind=c_int) i, j, k
   integer(kind=c_int), intent(in), value :: n
-  real(kind=c_double) :: val
   real(kind=c_double), dimension(1:n), intent(in) :: A
   real(kind=c_double), dimension(1:n, 1:n), intent(in) :: B
   real(kind=c_double), dimension(1:n, 1:n, 1:n), intent(in) :: C
-  real(kind=c_double), dimension(1:n, 1:n), intent(out) :: D
+  real(kind=c_double), dimension(1:n, 1:n), intent(inout) :: D
+  integer(kind=c_int) :: i, j, k
+  real(kind=c_double) :: val
 
-  do i = 1, n
-    do k = 1, n
-      do j = 1, n
-        if (i .eq. 1) then
+  do k = 1, n
+    do j = 1, n
+      do i = 1, n
+        if (i == 1) then
           val = B(j, k)
         else
           val = D(j, k)
         end if
-        D(j, k) = val + A(i) * C(j, k, i)
+        D(j, k) = val + A(i) * C(i, j, k)
       end do
     end do
   end do
--- Checks/PWR022/solution.f90
+++ Checks/PWR022/solution.f90
@@ -4,23 +4,22 @@ subroutine solution_f(n, A, B, C, D) bind(c)
   use iso_c_binding, only : c_int, c_double
 
   implicit none
-  integer(kind=c_int) i, j, k
   integer(kind=c_int), intent(in), value :: n
-  real(kind=c_double) :: val
   real(kind=c_double), dimension(1:n), intent(in) :: A
   real(kind=c_double), dimension(1:n, 1:n), intent(in) :: B
   real(kind=c_double), dimension(1:n, 1:n, 1:n), intent(in) :: C
-  real(kind=c_double), dimension(1:n, 1:n), intent(out) :: D
+  real(kind=c_double), dimension(1:n, 1:n), intent(inout) :: D
+  integer(kind=c_int) :: i, j, k
 
   do k = 1, n
     do j = 1, n
       D(j, k) = B(j, k) + A(1) * C(j, k, 1)
     end do
   end do
-  do i = 2, n
-    do k = 1, n
-      do j = 1, n
-        D(j, k) = D(j, k) + A(i) * C(j, k, i)
+  do k = 1, n
+    do j = 1, n
+      do i = 2, n
+        D(j, k) = D(j, k) + A(i) * C(i, j, k)
       end do
     end do
   end do
