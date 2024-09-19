d8e93884d5a13806ab1f7f7239e77e687a3b3473
Jaime Gonzalez <jaime.gonzalez@appentra.com>
2024-09-19 08:58:04 +0200

    PWD007: Update examples to use constant upperbound
    To be sure that PWR007 is detecting a valid recurrence, the loop
    upperbound should be constant, otherwise we can't be sure that
    left-hand-side and right-hand-side accessed indices overlap.

--- Checks/PWD007/example-omp.c
+++ Checks/PWD007/example-omp.c
@@ -1,10 +1,10 @@
 // PWD007: Unprotected multithreading recurrence
 
-void example(int *x, int *y, int size) {
+void example(int *x, int *y) {
   y[0] = 0;
 
   #pragma omp parallel for
-  for (int i = 1; i < size; i++) {
+  for (int i = 1; i < 100; i++) {
     y[i] = y[i - 1] + x[i - 1];
   }
 }
--- Checks/PWD007/example-omp.f90
+++ Checks/PWD007/example-omp.f90
@@ -9,7 +9,7 @@ subroutine example(x, y)
   y(1) = 0
 
   !$omp parallel do
-  do i = 2, size(y, 1)
+  do i = 2, 10
     y(i) = y(i - 1) + x(i - 1)
   end do
   !$omp end parallel do
