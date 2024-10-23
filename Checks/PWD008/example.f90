d82bc2cbc45d9e195f2b8d8277bd0c6f77e82115
alvrogd <alvaro.dieste@appentra.com>
2024-10-23 17:26:52 +0200

    PWD008: Add Fortran example code

---
+++ Checks/PWD008/example.f90
@@ -0,0 +1,14 @@
+! PWD008: Unprotected multithreading recurrence due to out-of-dimension-bounds
+! array access
+
+subroutine example(A)
+  integer, intent(inout) :: A(:, :)
+  integer :: i, j
+
+  !$omp parallel do
+  do j = 2, size(A, 2)
+    do i = 1, size(A, 1)
+      A(i, j) = A(i, j) + A(i - 1, j)
+    end do
+  end do 
+end subroutine example
