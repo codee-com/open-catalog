e5a2e06013cec0faa2491d87690d2a17582374f7
alvrogd <alvaro.dieste@appentra.com>
2025-01-02 15:06:45 +0100

    Web: Sort `DataTable` checks
    By default, alphabetical ordering is used. It will now follow the "PWR
    --> PWD --> RMK" order, just like in the original checks table.

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -11,6 +11,20 @@ const createDataTable = async () => {
     import('jquery'),
   ]);
 
+  // "PWR --> PWD --> RMK" sorting logic
+  // (sorted alphabetical by default)
+  const idPriority = { 'PWR': 1, 'PWD': 2, 'RMK': 3 };
+  const getIdPriority = (id) => idPriority[id.match(/^[A-Z]+/)] || 999;
+
+  jQuery.extend(DT.ext.type.order, {
+    'checks-asc': (a, b) => {
+      return getIdPriority(a) - getIdPriority(b);
+    },
+    'checks-des': (a, b) => {
+      return getIdPriority(b) - getIdPriority(a);
+    },
+  });
+
   const table = jQuery('#checks + table').first();
 
   // If found
@@ -20,6 +34,11 @@ const createDataTable = async () => {
 
     // Convert the checks table into a DataTable
     let dataTable = table.DataTable({
+      // Apply custom sorting logic
+      columnDefs: [
+        // ID
+        { 'targets': 0, 'type': 'checks' },
+      ],
       ordering: true,
       paging: false,
       searching: true,
