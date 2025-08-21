69b2a21a7a79f81897f1317d333e282bbd2ad499
Daniel Otero <daniel.otero@codee.com>
2025-08-21 08:57:24 +0200

    Benchmark: Prefer building google benchmark from source
    It doesn't take that much, and avoid linking problems when the library
    was compiled, for example, with a different C++ standard library.

--- Benchmark/cmake/modules/Benchmark.cmake
+++ Benchmark/cmake/modules/Benchmark.cmake
@@ -74,14 +74,14 @@ function(add_benchmark CHECKID)
   add_dependencies(run run-${CHECKID})
 endfunction()
 
-# Find a systems google benchmark library or build it from source
-# We require version 1.5.3 or above because we change the name of the benchmarks
-# through the `::Name()` method
-find_package(benchmark 1.5.3 QUIET)
-if (NOT benchmark_FOUND)
-  note("No compatible Benchmark library was found. Building it from source...")
-
+option(OCB_USE_SYSTEM_GBENCH "Prefer using the system installed google benchmark library, rather than compiling it from source" OFF)
+if (NOT OCB_USE_SYSTEM_GBENCH)
   # Disable benchmark testing to avoid depending on GTest
   set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "")
   add_subdirectory("${OCB_SOURCE_DIR}/external/google_benchmark")
+else()
+  # Find a systems google benchmark library or build it from source
+  # We require version 1.5.3 or above because we change the name of the benchmarks
+  # through the `::Name()` method
+  find_package(benchmark 1.5.3 REQUIRED)
 endif()
